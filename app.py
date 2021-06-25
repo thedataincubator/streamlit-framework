a4d065b955da77c6661ade4316ad2cd34a278dce
Svetlana Goncharenko <sgoncharenko1187@gmail.com>
2021-06-25 16:26:33 -0400

    getting data

--- app.py
+++ app.py
@@ -3,17 +3,39 @@ import streamlit as st
 
 ticker = st.text_input("Ticker")
 month = st.text_input("Month")
-day = st.text_input("Day")
 year = st.text_input("Year")
 
-st.text(year + " " + day + " " + month + " " + ticker)
+st.text(year + " " + month + " " + ticker)
 
 if st.button("Start"):
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&outputsize=full&apikey=5WB3BZG4UX3GQGAH'
     r = requests.get(url)
     data = r.json()
 
-    st.write(data)
+    timeSeries = data["Time Series (Daily)"]
+
+    results = []
+    day = 31
+    date = ""
+    while day > 0:
+        if day < 10:
+            date = year + "-" + month + "-0" + str(day)
+        else:
+            date = year + "-" + month + "-" + str(day)
+
+        if date not in timeSeries:
+            day = day - 1
+            continue
+
+        dayPrices = timeSeries[date]
+        closing = dayPrices["4. close"]
+        results.append(closing)
+
+        day = day - 1
+
+    st.write(results)
 else:
     st.write("Not yet started")
 
+
+data["Time series(Daily)"]["2021-06-24"]["4. close"]
