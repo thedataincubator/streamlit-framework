eb5ffd0195fed8fa3732d008cd0af5c301e22644
Svetlana Goncharenko <svetlana@Svetlanas-MBP.fios-router.home>
2021-06-25 15:40:42 -0400

    fix

--- app.py
+++ app.py
@@ -1,27 +1,5 @@
 import requests
 import streamlit as st
-import time
-import numpy as np
-
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
-
-#for i in range(1, 101):
-#     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-#     status_text.text("%i%% Complete" % i)
-#     chart.add_rows(new_rows)
-#     progress_bar.progress(i)
-#     last_rows = new_rows
-#     time.sleep(0.05)
-#
-# progress_bar.empty()
-
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
-st.button("Re-run")
 
 ticker = st.text_input("Ticker")
 month = st.text_input("Month")
@@ -30,8 +8,12 @@ year = st.text_input("Year")
 
 st.text(year + " " + day + " " + month + " " + ticker)
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&outputsize=full&apikey=5WB3BZG4UX3GQGAH'
-r = requests.get(url)
-data = r.json()
+if st.button("Start"):
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&outputsize=full&apikey=5WB3BZG4UX3GQGAH'
+    r = requests.get(url)
+    data = r.json()
 
-st.text(data)
+    st.write(data)
+else:
+    st.write("Not yet started")
+
