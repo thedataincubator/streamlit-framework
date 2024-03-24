f64a241bb3bc25dc9b13e10bfc62c568c890a568
Harriswells68 <136472037+Harriswells68@users.noreply.github.com>
2024-03-24 15:54:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,34 @@ def ai(data):
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response.text})
 
+from datetime import datetime
+from json import dumps
+
+from whatsapp_api_client_python import API
+
+greenAPI = API.GreenAPI(
+    "7103919868", "7f12e02c4c9b4b56b16a50efdb3d417cb4453b69d5314553ad"
+)
+
+def main():
+    greenAPI.webhooks.startReceivingNotifications(handler)
+
+def handler(type_webhook: str, body: dict) -> None:
+    if type_webhook == "incomingMessageReceived":
+        incoming_message_received(body)
+
+def incoming_message_received(body: dict) -> None:
+    timestamp = body["timestamp"]
+    time = get_notification_time(timestamp)
+
+    data = dumps(body, ensure_ascii=False, indent=4)
+    x = re.search(r'"textMessage":.*"', data)
+    message=(x.group().split(':')[1][2:(len(x.group().split(':')[1])-1)])
+    st.success(message)
+
+thread = threading.Thread(target=main, daemon=True)
+thread.start()
+
 import streamlit as st
 
 st.title("Gemini")
