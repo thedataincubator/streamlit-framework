7d510ff5408cae4ec3068def84ae0c4a2aa02ac4
Harriswells68 <136472037+Harriswells68@users.noreply.github.com>
2024-03-24 16:06:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -28,27 +28,6 @@ def ai(data):
 from datetime import datetime
 from json import dumps
 
-from whatsapp_api_client_python import API
-
-greenAPI = API.GreenAPI(
-    "7103919868", "7f12e02c4c9b4b56b16a50efdb3d417cb4453b69d5314553ad"
-)
-
-def main():
-    greenAPI.webhooks.startReceivingNotifications(handler)
-
-def handler(type_webhook: str, body: dict) -> None:
-    if type_webhook == "incomingMessageReceived":
-        incoming_message_received(body)
-
-def incoming_message_received(body: dict) -> None:
-    data = dumps(body, ensure_ascii=False, indent=4)
-    x = re.search(r'"textMessage":.*"', data)
-    message=(x.group().split(':')[1][2:(len(x.group().split(':')[1])-1)])
-    st.success(message)
-
-thread = threading.Thread(target=main, daemon=True)
-thread.start()
 
 import streamlit as st
 
@@ -78,3 +57,22 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         st.markdown(response)
     st.session_state.messages.append(k)
+from whatsapp_api_client_python import API
+
+greenAPI = API.GreenAPI(
+    "7103919868", "7f12e02c4c9b4b56b16a50efdb3d417cb4453b69d5314553ad"
+)
+import asyncio
+async def main():
+    greenAPI.webhooks.startReceivingNotifications(handler)
+
+def handler(type_webhook: str, body: dict) -> None:
+    if type_webhook == "incomingMessageReceived":
+        incoming_message_received(body)
+
+def incoming_message_received(body: dict) -> None:
+    data = dumps(body, ensure_ascii=False, indent=4)
+    x = re.search(r'"textMessage":.*"', data)
+    message=(x.group().split(':')[1][2:(len(x.group().split(':')[1])-1)])
+    st.success(message)
+asyncio.run(main())
