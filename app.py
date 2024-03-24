1995dd3701239e090854969e110479e645287332
Harriswells68 <136472037+Harriswells68@users.noreply.github.com>
2024-03-24 14:27:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -25,29 +25,6 @@ def ai(data):
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response.text})
 
-greenAPI = API.GreenAPI(
-    "7103919868", "7f12e02c4c9b4b56b16a50efdb3d417cb4453b69d5314553ad"
-)
-def main():
-    ai_chat(data="Hi Gemini, I've integrated your API with the WhatsApp API, and you're now connected to a WhatsApp group. You have the capability to interact with the group members by sending and receiving messages. Feel free to engage in conversations and provide assistance as needed. Enjoy your new environment and happy chatting!. Make sure to be short like we do in chatting")
-    greenAPI.webhooks.startReceivingNotifications(handler)
-
-
-def handler(type_webhook: str, body: dict) -> None:
-    if type_webhook == "incomingMessageReceived":
-        incoming_message_received(body)
-
-
-def incoming_message_received(body: dict) -> None:
-    data = dumps(body, ensure_ascii=False, indent=4)
-    x = re.search(r'"textMessage":.*"', data)
-    message=(x.group().split(':')[1][2:(len(x.group().split(':')[1])-1)])
-    if message:
-        with st.chat_message("user"):
-                    st.markdown(message)
-        st.session_state.messages.append({"role": "user", "content": message})
-        ai(data=f'New message recieved from Sujal: {message}')
-
 import streamlit as st
 
 st.title("Gemini")
@@ -76,5 +53,28 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         st.markdown(response)
     st.session_state.messages.append(k)
+
+greenAPI = API.GreenAPI(
+    "7103919868", "7f12e02c4c9b4b56b16a50efdb3d417cb4453b69d5314553ad"
+)
+def main():
+    ai_chat(data="Hi Gemini, I've integrated your API with the WhatsApp API, and you're now connected to a WhatsApp group. You have the capability to interact with the group members by sending and receiving messages. Feel free to engage in conversations and provide assistance as needed. Enjoy your new environment and happy chatting!. Make sure to be short like we do in chatting")
+    greenAPI.webhooks.startReceivingNotifications(handler)
+
+
+def handler(type_webhook: str, body: dict) -> None:
+    if type_webhook == "incomingMessageReceived":
+        incoming_message_received(body)
+
+
+def incoming_message_received(body: dict) -> None:
+    data = dumps(body, ensure_ascii=False, indent=4)
+    x = re.search(r'"textMessage":.*"', data)
+    message=(x.group().split(':')[1][2:(len(x.group().split(':')[1])-1)])
+    if message:
+        with st.chat_message("user"):
+                    st.markdown(message)
+        st.session_state.messages.append({"role": "user", "content": message})
+        ai(data=f'New message recieved from Sujal: {message}')
 listener_thread = threading.Thread(target=main, daemon=True)
 listener_thread.start()
