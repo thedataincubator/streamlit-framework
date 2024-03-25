3ff75451c7bbbc989a839d36e50ca0777051b666
Harriswells68 <136472037+Harriswells68@users.noreply.github.com>
2024-03-25 11:16:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ class Data(Base):
 record = session.query(Data).first()
 if record:
     # Import the dicmd dictionary
-    dicmd = record.dicmd
+    dicmd = record.dicmdr
+    record.message=[]
+    session.commit()
     record.message.extend([{"role": "user", "content": 'Hi', "id":None}, {"role": "assistant", "content": 'Hii,sujal', "id":'BAE5CC52E94351C2'}, {"role": "user", "content": 'halo', "id":None}, {"role": "assistant", "content": 'Hisujal', "id":None}])
     session.commit()
     messages=record.message
