1a499f60b1611500de7808c36c78bce0eec0c5aa
Harriswells68 <136472037+Harriswells68@users.noreply.github.com>
2024-03-25 13:17:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ if record:
     session.commit()
     k=[{"role": "user", "content": 'Hi', "id":None}, {"role": "assistant", "content": 'Hii,sujal', "id":'BAE5CC52E94351C2'}, {"role": "user", "content": 'halo', "id":None}, {"role": "assistant", "content": 'Hisujal', "id":None}]
     record.message.extend(k)
+    record.message=record.message
     print(record.message)
     session.commit()
     messages=record.message
